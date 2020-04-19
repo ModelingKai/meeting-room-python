10a1459302c4a375a7646467ec14bb6b4c39aeef
Jun Nakajima <ikuta1919@gmail.com>
2020-04-19 22:12:31 +0900

    freezegunを入れた

--- tests/domain/reservation/test_使用時間帯.py
+++ tests/domain/reservation/test_使用時間帯.py
@@ -1,3 +1,4 @@
+import freezegun
 import pytest
 
 from src.domain.reservation.errors import 使用時間帯の範囲がおかしいよError
@@ -5,10 +6,10 @@ from src.domain.reservation.使用日時 import 使用日時
 from src.domain.reservation.使用時間帯 import 使用時間帯
 
 
+@freezegun.freeze_time('2020-4-18')
 def test_予約できる時間帯は1000から1900までであること_異常系():
     with pytest.raises(使用時間帯の範囲がおかしいよError):
         使用時間帯(使用日時(2020, 4, 20, 9, 00), 使用日時(2020, 4, 20, 11, 00))
-
 
 def test_予約できる時間帯は1000から1900までであること_異常系2():
     with pytest.raises(使用時間帯の範囲がおかしいよError):
