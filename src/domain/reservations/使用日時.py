a9b5ce4fb8d49029f315b6cd1792fb0c63b3b532
Jun Nakajima <ikuta1919@gmail.com>
2020-04-19 20:33:27 +0900

    使用日時のルール1の正常系テストをグリーンにした。

---
+++ src/domain/reservations/使用日時.py
@@ -0,0 +1,8 @@
+class 使用日時:
+
+    def __init__(self, year: int, month: int, day: int, hour: int, minute: int):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
--- tests/domain/reservation/test_使用時間帯.py
+++ tests/domain/reservation/test_使用時間帯.py
@@ -1,6 +1,6 @@
-def test_使用時間帯():
-    assert False
+from src.domain.reservations.使用日時 import 使用日時
 
 
-def test_ルール1():
-    assert False
+def test_使用日時が15分単位であること():
+    jikan = 使用日時(2020, 4, 20, 10, 15)
+    assert jikan.minute == 15
