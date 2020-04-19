49280a82aa3ad4e5e2c86db4a90b50af8cf64ef3
mohira <shukatsudaison@gmail.com>
2020-04-19 20:44:45 +0900

    テスト実装: 「使用日時は15分区切りの15分単位でなければならない」

--- tests/domain/reservation/test_使用時間帯.py
+++ tests/domain/reservation/test_使用時間帯.py
@@ -1,6 +1,12 @@
+import pytest
+
 from src.domain.reservations.使用日時 import 使用日時
 
 
-def test_使用日時が15分単位であること():
-    jikan = 使用日時(2020, 4, 20, 10, 15)
-    assert jikan.minute == 15
+def test_使用日時が15分単位であること_正常系():
+    assert 使用日時(2020, 4, 20, 10, 15).minute % 15 == 0
+
+
+def test_使用日時が15分単位であること_異常系():
+    with pytest.raises(ValueError):
+        使用日時(2020, 4, 20, 10, 34).minute % 15 == 0
