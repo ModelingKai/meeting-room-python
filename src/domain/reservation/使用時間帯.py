cda3595281315b00e58ac71799cbcc4beade2b22
Jun Nakajima <ikuta1919@gmail.com>
2020-04-21 21:52:51 +0900

    使用時間帯のテストをグリーンにした
    - 過去の日付で予約ができないことのテスト
    - 14日後より先の予約ができないことのテスト
    
    # 気になる点
    - 14日後の変数名(later_14_days)
    - startとendそれぞれの日付で比較をしているが、現在の仕様では日を跨ぐことは無いので、条件式が冗長な気がする

--- src/domain/reservation/使用時間帯.py
+++ src/domain/reservation/使用時間帯.py
@@ -13,3 +13,10 @@ class 使用時間帯:
     def __post_init__(self):
         if not (datetime.time(10, 00) <= self.start.time() < self.end.time() <= datetime.time(19, 00)):
             raise 使用時間帯の範囲がおかしいよError('使用時間帯は10:00-19:00じゃないとダメだぞ！')
+
+        if self.start.date() < datetime.date.today() or self.end.date() < datetime.date.today():
+            raise 使用時間帯の範囲がおかしいよError('今日より過去の日付では予約できません')
+
+        later_14_days = datetime.datetime.now() + datetime.timedelta(days=14)
+        if later_14_days.date() < self.start.date() or later_14_days.date() < self.end.date():
+            raise 使用時間帯の範囲がおかしいよError('予約が可能な会議室は、14日後以内です')
