51974d2001d81f044cf170327105452a520b11af
Jun Nakajima <ikuta1919@gmail.com>
2020-04-22 20:21:19 +0900

    使用日時のテストをグリーンにした
    - freezgunを使用して日付を固定しようという案は、やめた
      - 使用日時に2100年という絶対的な未来を指定することによって、未来であることを明示的にする作戦
      - それによってfreezgunのメリットが薄れた為、テストコードがかさばるので、消した
    - freezgun限らず、モックライブラリは強力すぎる為、明確な理由が無い限りは使わなくていいようにしたい

--- tests/domain/reservation/test_使用日時.py
+++ tests/domain/reservation/test_使用日時.py
@@ -5,13 +5,12 @@ from src.domain.reservation.使用日時 import 使用日時
 
 
 def test_使用日時が15分単位であること_正常系():
-    assert 使用日時(2020, 4, 20, 10, 15).minute % 15 == 0
+    assert 使用日時(2100, 4, 20, 10, 15).minute % 15 == 0
 
 
 def test_使用日時が15分単位であること_異常系():
     with pytest.raises(Not15分単位Error):
-        使用日時(2020, 4, 20, 10, 34).minute % 15 == 0
-
+        使用日時(2100, 12, 31, 10, 34).minute % 15 == 0
 
 def test_使用日時は過去であってはならないこと():
     with pytest.raises(使用日時は過去であってはいけないんだよError):
