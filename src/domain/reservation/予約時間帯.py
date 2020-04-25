b7b87dad325587bc855a92a8fc9ab1d44a1aee66
mohira <shukatsudaison@gmail.com>
2020-04-25 23:03:37 +0900

    予約時間帯の重なりのテストケースを網羅した(たぶん)
    - テストを構造化しないとかなり見づらくなってきた
    - 時間帯の重なりパターンをコメントで表現できたと思う

--- src/domain/reservation/予約時間帯.py
+++ src/domain/reservation/予約時間帯.py
@@ -28,5 +28,12 @@ class 予約時間帯:
         return MAXIMUM_SECONDS < (self.end - self.start).total_seconds()
 
     def is_overlap(self, other: 予約時間帯) -> bool:
-        if self == other:  # 完全に一致しているトキ
+        if self == other:  # 完全に一致しているとき
             return True
+
+        is_先後_かぶりなし = (self.start < self.end <= other.start < other.end)
+        is_後先_かぶりなし = (other.start < other.end <= self.start < self.end)
+
+        is_かぶりなし = is_先後_かぶりなし or is_後先_かぶりなし
+
+        return not is_かぶりなし
