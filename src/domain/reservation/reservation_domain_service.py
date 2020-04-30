05ecad49f324e2f800fe7fe50966154d5a4cd233
mohira <shukatsudaison@gmail.com>
2020-04-30 21:06:07 +0900

    「会議室と時間帯的には予約できないけどキャンセル済みだから予約できること」のテストをGREENにした
    - `ReservationDomainService.can_not_reserve` 内の変数名がうまく付けられなかった
    - でも、テストが通っているからヨシ！

--- src/domain/reservation/reservation_domain_service.py
+++ src/domain/reservation/reservation_domain_service.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-from typing import List
 
 from src.domain.reservation.reservation import Reservation
 from src.domain.reservation.reservation_repository import ReservationRepository
+from src.domain.reservation.reservation_status import ReservationStatus
 
 
 @dataclass
@@ -10,11 +10,14 @@ class ReservationDomainService:
     reservation_repository: ReservationRepository
 
     def can_not_reserve(self, reservation: Reservation) -> bool:
-        # TODO: いずれは性能面を考えると、日付で検索した方がよさそう
-        reservations: List[Reservation] = self.reservation_repository.find_all()
+        # TODO: 性能面を考えると、クエリをカスタマイズしたほうがよさそう
+        all_reservations = self.reservation_repository.find_all()
 
-        for exist_reservation in reservations:
-            if exist_reservation.is_かぶり(reservation):
+        # 変数名的には NotCanceled だけど、フィルタ条件的には Reservedである なのはツラい
+        not_canceled_reservation = [r for r in all_reservations if r.reservation_status == ReservationStatus.Reserved]
+
+        for not_canceled_reservation in not_canceled_reservation:
+            if not_canceled_reservation.is_かぶり(reservation):
                 return True
 
         return False
