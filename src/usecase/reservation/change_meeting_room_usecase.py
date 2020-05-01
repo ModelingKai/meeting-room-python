beb8819f96578570583486781d252ffcc6841430
mohira <shukatsudaison@gmail.com>
2020-05-01 21:58:29 +0900

    `test_存在しない予約に対する会議室変更依頼はダメだよ` を実装(GREENだよ)
    - `TestCancelMeetingUsecase` と ほぼ同じテストだが、共通化はしません！
        - たまたま同じ内容になったけども、あくまで、ユースケースごとでみたら別のお話だからです
        - プロダクトコードもロジックが完全に同一だから共通化したくなるのは分かるけどね。

--- src/usecase/reservation/change_meeting_room_usecase.py
+++ src/usecase/reservation/change_meeting_room_usecase.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from src.domain.meeting_room.meeting_room_id import MeetingRoomId
 from src.domain.reservation.reservation_id import ReservationId
 from src.domain.reservation.reservation_repository import ReservationRepository
+from src.usecase.reservation.errors import NotFoundReservationError
 
 
 @dataclass
@@ -11,6 +12,9 @@ class ChangeMeetingRoomUseCase:
 
     def change_meeting_room(self, reservation_id: ReservationId, new_meeting_room_id: MeetingRoomId) -> None:
         reservation = self.reservation_repository.find_by_id(reservation_id)
+
+        if reservation is None:
+            raise NotFoundReservationError('そんな予約はないんやで？')
 
         changed_reservation = reservation.change_meeting_room(new_meeting_room_id)
 
--- tests/usecase/test_change_meeting_room_usecase.py
+++ tests/usecase/test_change_meeting_room_usecase.py
@@ -1,6 +1,7 @@
 import uuid
 
 import freezegun
+import pytest
 
 from src.domain.employee.employee_id import EmployeeId
 from src.domain.meeting_room.meeting_room_id import MeetingRoomId
@@ -11,6 +12,7 @@ from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
 from src.domain.reservation.使用日時 import 使用日時
 from src.infrastructure.reservation.in_memory_reservation_repository import InMemoryReservationRepository
 from src.usecase.reservation.change_meeting_room_usecase import ChangeMeetingRoomUseCase
+from src.usecase.reservation.errors import NotFoundReservationError
 
 
 class TestChangeMeetingRoomUsecase:
@@ -41,3 +43,17 @@ class TestChangeMeetingRoomUsecase:
         usecase.change_meeting_room(reservation.id, new_meeting_room_id)
 
         assert expected == repository.data[reservation.id]
+
+    @freezegun.freeze_time('2020-4-1 10:00')
+    def test_存在しない予約に対する会議室変更依頼はダメだよ(self):
+        reservation = Reservation(ReservationId(str(uuid.uuid4())),
+                                  TimeRangeToReserve(使用日時(2020, 4, 2, 13, 00), 使用日時(2020, 4, 2, 14, 00)),
+                                  NumberOfParticipants(4),
+                                  MeetingRoomId(str(uuid.uuid4())),
+                                  EmployeeId(str(uuid.uuid4())))
+
+        reservation_repository = InMemoryReservationRepository()
+        usecase = ChangeMeetingRoomUseCase(reservation_repository)
+
+        with pytest.raises(NotFoundReservationError):
+            usecase.change_meeting_room(reservation.id, MeetingRoomId(str(uuid.uuid4())))
