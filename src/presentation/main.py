1c73f1226466805f0d91e62f2ab49b9c27337670
Jun Nakajima <ikuta1919@gmail.com>
2020-05-04 22:24:20 +0900

    コンフリクトを解消した

--- src/presentation/main.py
+++ src/presentation/main.py
@@ -1,10 +1,17 @@
+import uuid
 from dataclasses import dataclass
 
 from orator import Schema, DatabaseManager
 from orator.schema import Blueprint
 
+from src.domain.employee.employee_id import EmployeeId
+from src.domain.meeting_room.meeting_room_id import MeetingRoomId
+from src.domain.reservation.number_of_participants import NumberOfParticipants
 from src.domain.reservation.reservation import Reservation
 from src.domain.reservation.reservation_domain_service import ReservationDomainService
+from src.domain.reservation.reservation_id import ReservationId
+from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
+from src.domain.reservation.使用日時 import 使用日時
 from src.infrastructure.reservation.orator.orator_reservation_repository import OratorReservationRepository
 from src.usecase.reservation.errors import その会議室はその時間帯では予約ができませんよエラー
 from src.usecase.reservation.reserve_meeting_room_usecase import ReserveMeetingRoomUsecase
@@ -39,22 +46,42 @@ def init_dev_db():
 
 
 @dataclass
-class UserInput:
-    input_date: str
-    input_start_time: str
-    input_end_time: str
-    input_meeting_room_id: str
-    input_reserver_id: str
-    input_number_of_participant: str
+class UserRawInput:
+    date: str
+    start_time: str
+    end_time: str
+    meeting_room_id: str
+    reserver_id: str
+    number_of_participants: str
 
 
 class SomeOne:
-    def to_reservation(self, user_input: UserInput) -> Reservation:
-        pass
+    def to_reservation(self, u: UserRawInput) -> Reservation:
+        year = int(u.date[:4])
+        month = int(u.date[4:6])
+        day = int(u.date[6:8])
+
+        start_hour = int(u.start_time[:2])
+        start_minute = int(u.start_time[2:4])
+
+        end_hour = int(u.end_time[:2])
+        end_minute = int(u.end_time[2:4])
+
+        start_使用日時 = 使用日時(year, month, day, start_hour, start_minute)
+        end_使用日時 = 使用日時(year, month, day, end_hour, end_minute)
+
+        reservation = Reservation(ReservationId(str(uuid.uuid4())),
+                                  TimeRangeToReserve(start_使用日時, end_使用日時),
+                                  NumberOfParticipants(int(u.number_of_participants)),
+                                  MeetingRoomId(u.meeting_room_id),
+                                  EmployeeId(u.reserver_id))
+
+        return reservation
 
 
 class ResponseObject:
-    pass
+    def __str__(self) -> str:
+        return 'なんかいい感じのメッセージ'
 
 
 class Darekaga:
@@ -64,7 +91,7 @@ class Darekaga:
 
 def main():
     # DB用意
-    # init_dev_db()
+    init_dev_db()
 
     # usecaseの準備
     database_manager = DatabaseManager(DEV_DB_CONFIG)
@@ -85,16 +112,16 @@ def main():
     # ここは一旦考えない
 
     # 2. ユースケースクラスに渡せるような形に変換する
-    user_input = UserInput(input_date,
-                           input_start_time,
-                           input_end_time,
-                           input_meeting_room_id,
-                           input_reserver_id,
-                           input_number_of_participants)
+    user_raw_input = UserRawInput(input_date,
+                                  input_start_time,
+                                  input_end_time,
+                                  input_meeting_room_id,
+                                  input_reserver_id,
+                                  input_number_of_participants)
 
     # ユーザからの文字列入力を、 Reservation に変換をする
     # to_reservation の責務は正しいドメインオブジェクトに変換をする
-    reservation: Reservation = SomeOne().to_reservation(user_input)
+    reservation: Reservation = SomeOne().to_reservation(user_raw_input)
 
     # 3. ユースケースに依頼する ← ユースケース層の世界で、あとはユースケースに任せる
     try:
