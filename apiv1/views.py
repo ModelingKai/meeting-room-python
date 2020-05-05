e8fafea2bc08edbe4c4c289b53502936851b56f7
Jun Nakajima <ikuta1919@gmail.com>
2020-05-05 21:21:31 +0900

    ReserveMeetingRoomAPIViewを追加 ここからどうユースケースを呼ぶべきか、すごく悩ましい

--- apiv1/views.py
+++ apiv1/views.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
 from shop.models import Book
-from .serializers import BookSerializer
+from src.usecase.reservation.reserve_meeting_room_usecase import ReserveMeetingRoomUsecase
+from .serializers import BookSerializer, ReserveSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -27,6 +28,32 @@ class BookCreateAPIView(views.APIView):
         # バリデーションを実行
         serializer.is_valid(raise_exception=True)
         # モデルオブジェクトを登録
+        # この時点でDatabaseに保存されるらしい
         serializer.save()
         # レスポンスオブジェクトを作成して返す
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class ReserveMeetingroomAPIView(views.APIView):
+    """
+    予約モデルの登録API
+    """
+
+    def post(self, request, *args, **kwargs):
+        input_date = '20200505'  # input('日付は？')
+        input_start_time = '1100'  # input('開始時刻は？')
+        input_end_time = '1200'  # input('終了時刻は？')
+        input_meeting_room_id = 'A'  # input('会議室は？')
+        input_reserver_id = 'Bob'  # input('あなたはだれ？')
+        input_number_of_participants = '4'
+
+        # リポジトリを用意する必要があるけど、誰に任せるか
+        # Oratorにまかせてみて、こっちのシリアライザとかは全く使わない？
+        # もしくは、中身でSerializerを使うRepositoryを作って、差し込むか？
+
+        # ユースケースを呼ぶ
+        usecase = ReserveMeetingRoomUsecase()
+        usecase.reserve_meeting_room(reservation)
+
+        # シリアライザオブジェクトを作成
+        serializer = ReserveSerializer(data=request.data)
