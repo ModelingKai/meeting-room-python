3bfa22167ec45b03924dda288f2ce8a9cd3c2ac6
Jun Nakajima <ikuta1919@gmail.com>
2020-05-05 16:36:00 +0900

    必要な引数を追加

--- apiv1/api.py
+++ apiv1/api.py
@@ -5,8 +5,13 @@ from django.http.response import JsonResponse
 
 def json_reserve(request):
 
+    input_date = '20200505'  # input('日付は？')
+    input_start_time = '1100'  # input('開始時刻は？')
+    input_end_time = '1200'  # input('終了時刻は？')
+    input_meeting_room_id = 'A'  # input('会議室は？')
+    input_reserver_id = 'Bob'  # input('あなたはだれ？')
+    input_number_of_participants = '4'
 
-    id_ = 'datades'
     return JsonResponse(
         data={
             "question": "結果ですよpost"
