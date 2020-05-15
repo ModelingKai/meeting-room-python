2ea43525eef280fd249d3f3d805319b0d6cfe414
Jun Nakajima <ikuta1919@gmail.com>
2020-05-15 21:10:10 +0900

    呼び出し側のコードを、本物に近づけた
    - MockFindMeetingRoomUsecase を本物に置き換えた
      - 中身で使っている Factory と Repository はまだ偽物です

--- src/presentation/cli/user_operation/reserve_meeting_room.py
+++ src/presentation/cli/user_operation/reserve_meeting_room.py
@@ -3,13 +3,16 @@ import datetime
 from orator import DatabaseManager
 
 from src.app_environment.init_dev_db import DEV_DB_CONFIG, init_dev_db
+from src.domain.meeting_room.meeting_room import MeetingRoom
+from src.domain.meeting_room.meeting_room_id_factory import MeetingRoomIdFactory
 from src.domain.reservation.errors import ReservationDomainObjectError
 from src.domain.reservation.reservation_domain_service import ReservationDomainService
+from src.infrastructure.meeting_room.in_memory_meeting_room_repository import InMemoryMeetingRoomRepository
 from src.infrastructure.reservation.orator.orator_reservation_repository import OratorReservationRepository
 from src.presentation.cli.cli_util.response_object_factory import ResponseObjectFactory
 from src.presentation.cli.cli_util.user_input import CliUserInput
 from src.usecase.employee.mock_find_employee_usecase import MockFindEmployeeUseCase
-from src.usecase.meeting_room.mock_find_meeting_room_usecase import MockFindMeetingRoomUseCase
+from src.usecase.meeting_room.find_meeting_room_usecase import FindMeetingRoomUseCase
 from src.usecase.reservation.errors import ReservationUsecaseError
 from src.usecase.reservation.reserve_meeting_room_usecase import ReserveMeetingRoomUsecase
 
@@ -125,19 +128,26 @@ def 新規予約():
     usecase = ReserveMeetingRoomUsecase(reservation_repository, domain_service)
 
     init_dev_db()  # 検証しやすくするために毎回DBを初期化する
-    input_使用日 = Task使用日().exe()
-    input_開始時刻 = Task開始時刻().exe()
-    input_終了時刻 = Task終了時刻().exe()
-    input_会議室ID = Task会議室ID().exe()
-    input_社員ID = Task社員ID().exe()
-    input_使用人数 = Task使用人数().exe()
+    # input_使用日 = Task使用日().exe()
+    # input_開始時刻 = Task開始時刻().exe()
+    # input_終了時刻 = Task終了時刻().exe()
+    # input_会議室ID = Task会議室ID().exe()
+    # input_社員ID = Task社員ID().exe()
+    # input_使用人数 = Task使用人数().exe()
 
-    user_input = CliUserInput(input_使用日,
-                              input_開始時刻,
-                              input_終了時刻,
-                              input_会議室ID,
-                              input_社員ID,
-                              input_使用人数)
+    # user_input = CliUserInput(input_使用日,
+    #                           input_開始時刻,
+    #                           input_終了時刻,
+    #                           input_会議室ID,
+    #                           input_社員ID,
+    #                           input_使用人数)
+
+    user_input = CliUserInput('20200516',
+                              '1100',
+                              '1300',
+                              'A',
+                              '001',
+                              '5')
 
     try:
         reservation = user_input.to_reservation()
@@ -152,7 +162,14 @@ def 新規予約():
         exit()
 
     # TODO: Mockのユースケースを本物に差し替える
-    factory = ResponseObjectFactory(MockFindEmployeeUseCase(), MockFindMeetingRoomUseCase())
+    meeting_room_repository = InMemoryMeetingRoomRepository()
+    factory = MeetingRoomIdFactory(meeting_room_repository)
+    meeting_room_id = factory.create('A')
+    meeting_room = MeetingRoom(meeting_room_id, '大会議室')
+
+    meeting_room_repository.data[meeting_room_id] = meeting_room
+
+    factory = ResponseObjectFactory(MockFindEmployeeUseCase(), FindMeetingRoomUseCase(meeting_room_repository))
     response_object = factory.create(reservation)
 
     print(response_object)
