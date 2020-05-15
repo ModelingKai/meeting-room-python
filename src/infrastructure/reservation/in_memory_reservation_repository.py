1abe882c34c87c25e9680c5da82af631e47e1c06
mohira <shukatsudaison@gmail.com>
2020-05-15 21:47:38 +0900

    InMemoryReservationRepository の data を インスタンス変数にした

--- src/infrastructure/reservation/in_memory_reservation_repository.py
+++ src/infrastructure/reservation/in_memory_reservation_repository.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Union
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
 
 from src.domain.reservation.reservation import Reservation
 from src.domain.reservation.reservation_id import ReservationId
@@ -6,8 +7,9 @@ from src.domain.reservation.reservation_repository import ReservationRepository
 from src.domain.reservation.reservation_specification import ReservationSpecification
 
 
+@dataclass
 class InMemoryReservationRepository(ReservationRepository):
-    data: Dict[ReservationId, Reservation] = {}
+    data: Dict[ReservationId, Reservation] = field(default_factory=dict)
 
     def reserve_new_meeting_room(self, reservation: Reservation) -> None:
         self.data[reservation.id] = reservation
@@ -17,7 +19,7 @@ class InMemoryReservationRepository(ReservationRepository):
 
         return available_reservations
 
-    def find_by_id(self, reservation_id: ReservationId) -> Union[Reservation, None]:
+    def find_by_id(self, reservation_id: ReservationId) -> Optional[Reservation]:
         return self.data.get(reservation_id)
 
     def change_meeting_room(self, reservation: Reservation) -> None:
