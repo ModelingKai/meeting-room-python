1e7727c4295261435df01535beea582e0e41776c
mohira <shukatsudaison@gmail.com>
2020-05-15 23:30:53 +0900

    Chore: factory -> id_factory

--- tests/domain/test_meeting_room_id_factory.py
+++ tests/domain/test_meeting_room_id_factory.py
@@ -10,14 +10,14 @@ from src.infrastructure.meeting_room.in_memory_meeting_room_repository import In
 class TestMeetingRoomIdFactory:
     def setup(self):
         self.repository = InMemoryMeetingRoomRepository()
-        self.factory = MeetingRoomIdFactory(self.repository)
+        self.id_factory = MeetingRoomIdFactory(self.repository)
 
     def test_存在しない会議室IDが与えられたらエラーとなる(self):
         with pytest.raises(NotFoundMeetingRoomIdError):
-            self.factory.create('NotExistId')
+            self.id_factory.create('NotExistId')
 
     def test_正しい会議室IDを作れる(self):
         meeting_room_id = MeetingRoomId('A')
         self.repository.data[meeting_room_id] = MeetingRoom(meeting_room_id, '大会議室')
 
-        assert meeting_room_id == self.factory.create('A')
+        assert meeting_room_id == self.id_factory.create('A')
