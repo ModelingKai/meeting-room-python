51fb596b790c0385d2ecc421ab2fc65ab84edfcc
mohira <shukatsudaison@gmail.com>
2020-05-15 23:28:32 +0900

    ユースケースの実行にコマンドオブジェクトを利用することにした。
    - ID生成にファクトリを使っているので、MeetingRoomRepository.find_by_id() は信用できる！
    - 実装者にファクトリ利用を100%強要することはできないが、レビューとかで対処していこうな！
    - また、 `NotFoundMeetingRoomError` を残すと、テストがしにくい！ => 設計がヤバい気がする...!!!

--- src/usecase/meeting_room/find_meeting_room_usecase.py
+++ src/usecase/meeting_room/find_meeting_room_usecase.py
@@ -1,11 +1,8 @@
 from dataclasses import dataclass
 
-from src.domain.meeting_room.errors import NotFoundMeetingRoomIdError
 from src.domain.meeting_room.meeting_room import MeetingRoom
-from src.domain.meeting_room.meeting_room_id import MeetingRoomId
 from src.domain.meeting_room.meeting_room_id_factory import MeetingRoomIdFactory
 from src.domain.meeting_room.meeting_room_repository import MeetingRoomRepository
-from src.usecase.meeting_room.errors import NotFoundMeetingRoomError
 from src.usecase.meeting_room.find_meeting_room_commnad import FindMeetingRoomCommand
 
 
@@ -13,24 +10,9 @@ from src.usecase.meeting_room.find_meeting_room_commnad import FindMeetingRoomCo
 class FindMeetingRoomUseCase:
     repository: MeetingRoomRepository
 
-    def find_by_id(self, meeting_room_id: MeetingRoomId) -> MeetingRoom:
-        meeting_room = self.repository.find_by_id(meeting_room_id)
+    def find_meeting_room(self, command: FindMeetingRoomCommand) -> MeetingRoom:
+        id_factory = MeetingRoomIdFactory(self.repository)
+        meeting_room_id = id_factory.create(command.meeting_room_id)
 
-        if meeting_room is None:
-            raise NotFoundMeetingRoomError('そんな会議室はありませんぜ')
-
-        return meeting_room
-
-    def find_employee(self, command: FindMeetingRoomCommand) -> MeetingRoom:
-        try:
-            id_factory = MeetingRoomIdFactory(self.repository)
-            meeting_room_id = id_factory.create(command.meeting_room_id)  # 例外処理はどこで担当？
-        except NotFoundMeetingRoomIdError:
-            raise NotFoundMeetingRoomError()  # ユースケースエラーを送出し直す？
-
-        meeting_room = self.repository.find_by_id(meeting_room_id)
-
-        if meeting_room is None:
-            raise NotFoundMeetingRoomError('そんな会議室はありませんぜ')
-
-        return meeting_room
+        # ID生成用のファクトリを使っているので、Noneになることはないんだぞ
+        return self.repository.find_by_id(meeting_room_id)
--- tests/usecase/meeting_room/test_find_meeting_room_usecase.py
+++ tests/usecase/meeting_room/test_find_meeting_room_usecase.py
@@ -1,3 +1,6 @@
+import pytest
+
+from src.domain.meeting_room.errors import NotFoundMeetingRoomIdError
 from src.domain.meeting_room.meeting_room import MeetingRoom
 from src.domain.meeting_room.meeting_room_id import MeetingRoomId
 from src.domain.meeting_room.meeting_room_id_factory import MeetingRoomIdFactory
@@ -19,14 +22,13 @@ class TestFindMeetingRoomUsecase:
 
         self.repository.data[meeting_room_id] = meeting_room
 
-        assert meeting_room == self.usecase.find_by_id(meeting_room_id)
-
-    def test_コマンドオブジェクトをつかうとFactory問題も同時に解決する説(self):
-        meeting_room_id = MeetingRoomId('A')
-        meeting_room = MeetingRoom(meeting_room_id, '大会議室')
-
-        self.repository.data[meeting_room_id] = meeting_room
-
         command = FindMeetingRoomCommand('A')
 
-        assert meeting_room == self.usecase.find_employee(command)
+        assert meeting_room == self.usecase.find_meeting_room(command)
+
+    def test_存在しない会議室IDを渡すとエラーになる(self):
+        command = FindMeetingRoomCommand('NotExistMeetingRoomId')
+
+        # UsecaseErrorではない点に注意
+        with pytest.raises(NotFoundMeetingRoomIdError):
+            self.usecase.find_meeting_room(command)
