d951b15f549348525bb6484d1e97b5592c75799d
Jun Nakajima <ikuta1919@gmail.com>
2020-05-17 00:34:57 +0900

    reserver_name をユースケースを利用して取得するようにした
    - employee と reserver が結構ややこしい
      - 集約的には正しいんだが…

--- src/presentation/cli/cli_util/success_message_builder.py
+++ src/presentation/cli/cli_util/success_message_builder.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 
 from src.domain.reservation.reservation import Reservation
 from src.presentation.cli.cli_util.success_message import SuccessMessage
+from src.usecase.employee.find_employee_command import FindEmployeeCommand
+from src.usecase.employee.find_employee_usecase import FindEmployeeUseCase
 from src.usecase.meeting_room.find_meeting_room_commnad import FindMeetingRoomCommand
 from src.usecase.meeting_room.find_meeting_room_usecase import FindMeetingRoomUseCase
 
@@ -10,11 +12,11 @@ from src.usecase.meeting_room.find_meeting_room_usecase import FindMeetingRoomUs
 @dataclass
 class SuccessMessageBuilder:
     find_meeting_room_usecase: FindMeetingRoomUseCase
+    find_employee_usecase: FindEmployeeUseCase
 
     def build(self, reservation: Reservation) -> SuccessMessage:
         meeting_room_name = self.find_meeting_room_name(reservation)
-
-        reserver_name = 'Bob'
+        reserver_name = self.find_reserver_name(reservation)
         number_of_participants = reservation.number_of_participants.value
 
         year = 2020
@@ -33,6 +35,12 @@ class SuccessMessageBuilder:
             reserver_name,
             number_of_participants
         )
+
+    def find_reserver_name(self, reservation: Reservation) -> str:
+        find_employee_command = FindEmployeeCommand(employee_id=reservation.reserver_id.value)
+        reserver = self.find_employee_usecase.find_employee(find_employee_command)
+
+        return reserver.name
 
     def find_meeting_room_name(self, reservation: Reservation) -> str:
         find_meeting_room_command = FindMeetingRoomCommand(meeting_room_id=reservation.meeting_room_id.value)
--- tests/presentation/cli/test_success_message_builder.py
+++ tests/presentation/cli/test_success_message_builder.py
@@ -3,6 +3,8 @@ import uuid
 import freezegun
 import pytest
 
+from src.domain.employee.employee import Employee
+from src.domain.employee.employee_domain_service import EmployeeDomainService
 from src.domain.employee.employee_id import EmployeeId
 from src.domain.meeting_room.meeting_room import MeetingRoom
 from src.domain.meeting_room.meeting_room_domain_service import MeetingRoomDomainService
@@ -12,8 +14,10 @@ from src.domain.reservation.reservation import Reservation
 from src.domain.reservation.reservation_id import ReservationId
 from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
 from src.domain.reservation.使用日時 import 使用日時
+from src.infrastructure.employee.in_memory_employee_repository import InMemoryEmployeeRepository
 from src.infrastructure.meeting_room.in_memory_meeting_room_repository import InMemoryMeetingRoomRepository
 from src.presentation.cli.cli_util.success_message_builder import SuccessMessageBuilder
+from src.usecase.employee.find_employee_usecase import FindEmployeeUseCase
 from src.usecase.meeting_room.find_meeting_room_usecase import FindMeetingRoomUseCase
 
 
@@ -38,8 +42,17 @@ class TestSuccessMessageBuilder:
 
         meeting_room_repository.data[meeting_room_id] = meeting_room
 
+        # Employee に関する準備
+        employee_repository = InMemoryEmployeeRepository()
+        employee_domain_service = EmployeeDomainService(employee_repository)
+        find_employee_usecase = FindEmployeeUseCase(employee_repository, employee_domain_service)
+
+        employee_id = EmployeeId('001')
+        employee = Employee(employee_id, 'Bob')
+        employee_repository.data[employee_id] = employee
+
         # こっから本編
-        success_message_builder = SuccessMessageBuilder(find_meeting_room_usecase)
+        success_message_builder = SuccessMessageBuilder(find_meeting_room_usecase, find_employee_usecase)
 
         success_message = success_message_builder.build(reservation)
 
