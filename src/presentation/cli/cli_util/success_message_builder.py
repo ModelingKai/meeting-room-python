697a8c2eea92f5f15a53b62a45ed7056d84dfe94
mohira <shukatsudaison@gmail.com>
2020-05-17 00:14:35 +0900

    SuccessMessage の初期化用の値を変数に抽出して考えやすくする作戦を実行

--- src/presentation/cli/cli_util/success_message_builder.py
+++ src/presentation/cli/cli_util/success_message_builder.py
@@ -6,13 +6,23 @@ from src.presentation.cli.cli_util.success_message import SuccessMessage
 
 class SuccessMessageBuilder:
     def build(self, reservation: Reservation) -> SuccessMessage:
+        meeting_room_name = '大会議室'
+        reserver_name = 'Bob'
+        number_of_participants = reservation.number_of_participants.value
+
+        year = 2020
+        month = 4
+        day = 2
+        start_time = datetime.time(13, 00)
+        end_time = datetime.time(14, 00)
+
         return SuccessMessage(
-            2020,
-            4,
-            2,
-            datetime.time(13, 00),
-            datetime.time(14, 00),
-            '大会議室',
-            'Bob',
-            4
+            year,
+            month,
+            day,
+            start_time,
+            end_time,
+            meeting_room_name,
+            reserver_name,
+            number_of_participants
         )
