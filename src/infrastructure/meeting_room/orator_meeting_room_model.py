cc08bb73b5711d8b13f659c43b4fe248bc555abe
Jun Nakajima <ikuta1919@gmail.com>
2020-05-19 21:43:20 +0900

    OratorMeetingRoomRepository.find_by_id のテストを通した
    - Oratorモデルのカラムが、migrationファイルの方で定義見ないといけないのが、若干わかりづらかった。
      - 慣れればどうってことは無さそう。
      - Django使っていた人から見ると、最初はわからんかった。

--- src/infrastructure/meeting_room/orator_meeting_room_model.py
+++ src/infrastructure/meeting_room/orator_meeting_room_model.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+
 from orator import Model
+
+from src.domain.meeting_room.meeting_room import MeetingRoom
+from src.domain.meeting_room.meeting_room_id import MeetingRoomId
 
 
 class OratorMeetingRoomModel(Model):
@@ -10,3 +15,15 @@ class OratorMeetingRoomModel(Model):
         repr_like_dataclass = f'{self.__class__.__name__}({tmp})'
 
         return repr_like_dataclass
+
+    @classmethod
+    def to_meeting_room(cls, source: OratorMeetingRoomModel) -> MeetingRoom:
+        return MeetingRoom(MeetingRoomId(source.id), source.name)
+
+    @classmethod
+    def to_orator_model(cls, meeting_room: MeetingRoom) -> OratorMeetingRoomModel:
+        orator_meeting_room = OratorMeetingRoomModel()
+        orator_meeting_room.id = meeting_room.id.value
+        orator_meeting_room.name = meeting_room.name
+
+        return orator_meeting_room
--- tests/infrastructure/meeting_room/test_orator_meeting_room_repository.py
+++ tests/infrastructure/meeting_room/test_orator_meeting_room_repository.py
@@ -1,24 +1,41 @@
 from typing import List, Optional
 
+from orator import DatabaseManager, Model
+
 from src.domain.meeting_room.meeting_room import MeetingRoom
 from src.domain.meeting_room.meeting_room_id import MeetingRoomId
 from src.domain.meeting_room.meeting_room_repository import MeetingRoomRepository
+from src.infrastructure.meeting_room.orator_meeting_room_model import OratorMeetingRoomModel
+from tests.usecase.reservation.orator.migrate_in_memory import migrate_in_memory, TEST_DB_CONFIG
 
 
 class OratorMeetingRoomRepository(MeetingRoomRepository):
 
     def find_by_id(self, meeting_room_id: MeetingRoomId) -> Optional[MeetingRoom]:
-        pass
+        orator_meeting_room = OratorMeetingRoomModel.find(meeting_room_id.value)
+
+        if orator_meeting_room is None:
+            return None
+
+        return OratorMeetingRoomModel.to_meeting_room(orator_meeting_room)
 
     def find_all(self) -> List[MeetingRoom]:
         pass
 
 
 class TestOratorMeetingRoomRepository:
+    def setup(self):
+        database_manager = DatabaseManager(TEST_DB_CONFIG)
+        Model.set_connection_resolver(database_manager)
+
+        migrate_in_memory(database_manager)
+
+        self.repository = OratorMeetingRoomRepository()
+
     def test_find_by_id(self):
-        repository = OratorMeetingRoomRepository()
-
         meeting_room_id = MeetingRoomId('A')
-        actual = repository.find_by_id(meeting_room_id)
+        meeting_room = MeetingRoom(meeting_room_id, '大会議室')
 
-        assert MeetingRoom(meeting_room_id, '大会議室') == actual
+        OratorMeetingRoomModel.to_orator_model(meeting_room).save()
+
+        assert self.repository.find_by_id(meeting_room_id) == meeting_room
