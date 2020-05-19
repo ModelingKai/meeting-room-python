1b441a935425b8be1838931c8b3d372302afb304
mohira <shukatsudaison@gmail.com>
2020-05-19 21:52:35 +0900

    TestOratorMeetingRoomRepository.find_all() のテストを実装した
    - リスト内包表記 か list(map(...)) はどっちでも良さそうだが、ReservationRepositoryで list(map()) にしてたので、そっちに合わせた
    - テストメソッドの独立性を高めるために、setup()内で、データを追加することはやめたよ

--- tests/infrastructure/meeting_room/test_orator_meeting_room_repository.py
+++ tests/infrastructure/meeting_room/test_orator_meeting_room_repository.py
@@ -20,7 +20,7 @@ class OratorMeetingRoomRepository(MeetingRoomRepository):
         return OratorMeetingRoomModel.to_meeting_room(orator_meeting_room)
 
     def find_all(self) -> List[MeetingRoom]:
-        pass
+        return list(map(OratorMeetingRoomModel.to_meeting_room, OratorMeetingRoomModel.all()))
 
 
 class TestOratorMeetingRoomRepository:
@@ -33,12 +33,22 @@ class TestOratorMeetingRoomRepository:
         self.repository = OratorMeetingRoomRepository()
 
     def test_find_by_id(self):
-        meeting_room_id = MeetingRoomId('A')
-        meeting_room = MeetingRoom(meeting_room_id, '大会議室')
+        meeting_room = MeetingRoom(MeetingRoomId('A'), '大会議室')
 
         OratorMeetingRoomModel.to_orator_model(meeting_room).save()
 
-        assert self.repository.find_by_id(meeting_room_id) == meeting_room
+        assert self.repository.find_by_id(meeting_room.id) == meeting_room
 
     def test_find_by_id_return_None_when_not_exist_id(self):
         assert self.repository.find_by_id(MeetingRoomId('Z')) is None
+
+    def test_find_all(self):
+        meeting_room_a = MeetingRoom(MeetingRoomId('A'), '大会議室')
+        meeting_room_b = MeetingRoom(MeetingRoomId('B'), '中会議室')
+        meeting_room_c = MeetingRoom(MeetingRoomId('C'), '小会議室')
+
+        OratorMeetingRoomModel.to_orator_model(meeting_room_a).save()
+        OratorMeetingRoomModel.to_orator_model(meeting_room_b).save()
+        OratorMeetingRoomModel.to_orator_model(meeting_room_c).save()
+
+        assert self.repository.find_all() == [meeting_room_a, meeting_room_b, meeting_room_c]
