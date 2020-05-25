764cfc512736603194d61cb54965ed3ae7cdfbaa
mohira <shukatsudaison@gmail.com>
2020-05-25 22:31:29 +0900

    Move: ReservationFactory は 集約をまたいだ処理をしているが、メインとなるのはReservation なので、domain/reservation に配置した

---
+++ src/domain/reservation/reservation_factory.py
@@ -0,0 +1,56 @@
+import uuid
+from dataclasses import dataclass
+
+from src.domain.employee.employee_id import EmployeeId
+from src.domain.employee.employee_repository import EmployeeRepository
+from src.domain.employee.errors import NotFoundEmployeeIdError
+from src.domain.meeting_room.meeting_room_id import MeetingRoomId
+from src.domain.meeting_room.meeting_room_repository import MeetingRoomRepository
+from src.domain.reservation.number_of_participants import NumberOfParticipants
+from src.domain.reservation.reservation import Reservation
+from src.domain.reservation.reservation_id import ReservationId
+from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
+from src.domain.reservation.使用日時 import 使用日時
+from src.usecase.meeting_room.errors import NotFoundMeetingRoomIdError
+
+
+@dataclass
+class ReservationFactory(object):
+    meeting_room_repository: MeetingRoomRepository
+    employee_repository: EmployeeRepository
+
+    def create(self,
+               date: str,
+               start_time: str,
+               end_time: str,
+               meeting_room_id: str,
+               reserver_id: str,
+               number_of_participants: str,
+               ) -> Reservation:
+
+        employee = self.employee_repository.find_by_id(EmployeeId(reserver_id))
+
+        if employee is None:
+            raise NotFoundEmployeeIdError('そんな社員IDはありませんよ')
+
+        meeting_room = self.meeting_room_repository.find_by_id(MeetingRoomId(meeting_room_id))
+
+        if meeting_room is None:
+            raise NotFoundMeetingRoomIdError('そんな会議室IDはありませんよ')
+
+        year, month, day = int(date[:4]), int(date[4:6]), int(date[6:8])
+
+        start_hour = int(start_time[:2])
+        start_minute = int(start_time[2:4])
+
+        end_hour = int(end_time[:2])
+        end_minute = int(end_time[2:4])
+
+        start_使用日時 = 使用日時(year, month, day, start_hour, start_minute)
+        end_使用日時 = 使用日時(year, month, day, end_hour, end_minute)
+
+        return Reservation(ReservationId(str(uuid.uuid4())),
+                           TimeRangeToReserve(start_使用日時, end_使用日時),
+                           NumberOfParticipants(int(number_of_participants)),
+                           meeting_room.id,
+                           employee.id)
--- tests/domain/reservation/test_reservation_factory.py
+++ tests/domain/reservation/test_reservation_factory.py
@@ -1,73 +1,18 @@
-import uuid
-from dataclasses import dataclass
-
 import freezegun
 import pytest
 
 from src.domain.employee.employee import Employee
 from src.domain.employee.employee_id import EmployeeId
-from src.domain.employee.employee_repository import EmployeeRepository
 from src.domain.employee.errors import NotFoundEmployeeIdError
 from src.domain.meeting_room.meeting_room import MeetingRoom
 from src.domain.meeting_room.meeting_room_id import MeetingRoomId
-from src.domain.meeting_room.meeting_room_repository import MeetingRoomRepository
-from src.domain.reservation.number_of_participants import NumberOfParticipants
-from src.domain.reservation.reservation import Reservation
-from src.domain.reservation.reservation_id import ReservationId
-from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
-from src.domain.reservation.使用日時 import 使用日時
+from src.domain.reservation.reservation_factory import ReservationFactory
 from src.infrastructure.employee.in_memory_employee_repository import InMemoryEmployeeRepository
 from src.infrastructure.meeting_room.in_memory_meeting_room_repository import InMemoryMeetingRoomRepository
 from src.usecase.meeting_room.errors import NotFoundMeetingRoomIdError
 
 
-@dataclass
-class ReservationFactory(object):
-    meeting_room_repository: MeetingRoomRepository
-    employee_repository: EmployeeRepository
-
-    def create(self,
-               date: str,
-               start_time: str,
-               end_time: str,
-               meeting_room_id: str,
-               reserver_id: str,
-               number_of_participants: str,
-               ):
-
-        employee = self.employee_repository.find_by_id(EmployeeId(reserver_id))
-
-        if employee is None:
-            raise NotFoundEmployeeIdError('そんな社員IDはありませんよ')
-
-        meeting_room = self.meeting_room_repository.find_by_id(MeetingRoomId(meeting_room_id))
-
-        if meeting_room is None:
-            raise NotFoundMeetingRoomIdError('そんな会議室IDはありませんよ')
-
-        year = int(date[:4])
-        month = int(date[4:6])
-        day = int(date[6:8])
-
-        start_hour = int(start_time[:2])
-        start_minute = int(start_time[2:4])
-
-        end_hour = int(end_time[:2])
-        end_minute = int(end_time[2:4])
-
-        start_使用日時 = 使用日時(year, month, day, start_hour, start_minute)
-        end_使用日時 = 使用日時(year, month, day, end_hour, end_minute)
-
-        reservation = Reservation(ReservationId(str(uuid.uuid4())),
-                                  TimeRangeToReserve(start_使用日時, end_使用日時),
-                                  NumberOfParticipants(int(number_of_participants)),
-                                  meeting_room_id,
-                                  EmployeeId(reserver_id))
-
-        return reservation
-
-
-class TestReservation:
+class TestReservationFactory:
     def setup(self):
         employee_repository = InMemoryEmployeeRepository()
         employee_id = EmployeeId('001')
