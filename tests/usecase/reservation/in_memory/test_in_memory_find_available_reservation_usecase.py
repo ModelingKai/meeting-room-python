0a20dc7caaaeda042c55ac9add303fc7f87b4001
mohira <shukatsudaison@gmail.com>
2020-05-28 22:51:03 +0900

    Delete: TestInMemoryReservationRepository は TestInMemoryFindAvailableReservationsUsecase で賄えているので削除。

--- tests/usecase/reservation/in_memory/test_in_memory_find_available_reservation_usecase.py
+++ tests/usecase/reservation/in_memory/test_in_memory_find_available_reservation_usecase.py
@@ -1,13 +1,12 @@
 import dataclasses
-import datetime
 import uuid
 
 import freezegun
 import pytest
+from orator import DatabaseManager, Model
 
 from src.domain.employee.employee_id import EmployeeId
 from src.domain.meeting_room.meeting_room_id import MeetingRoomId
-from src.domain.reservation.available_reservation_specification import AvailableReservationSpecification
 from src.domain.reservation.number_of_participants import NumberOfParticipants
 from src.domain.reservation.reservation import Reservation
 from src.domain.reservation.reservation_id import ReservationId
@@ -15,9 +14,17 @@ from src.domain.reservation.reservation_status import ReservationStatus
 from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
 from src.domain.reservation.使用日時 import 使用日時
 from src.infrastructure.reservation.in_memory_reservation_repository import InMemoryReservationRepository
+from src.usecase.reservation.find_avalible_reservation_usecase import FindAvailableReservationsUsecase
+from tests.usecase.reservation.orator.migrate_in_memory import TEST_DB_CONFIG, migrate_in_memory
 
 
-class TestInMemoryReservationRepository:
+class TestInMemoryFindAvailableReservationsUsecase:
+    def setup(self):
+        database_manager = DatabaseManager(TEST_DB_CONFIG)
+
+        Model.set_connection_resolver(database_manager)
+
+        migrate_in_memory(database_manager)
 
     @pytest.fixture
     @freezegun.freeze_time('2020-4-1 10:00')
@@ -38,20 +45,18 @@ class TestInMemoryReservationRepository:
                            EmployeeId('001'))
 
     @freezegun.freeze_time('2020-4-1 10:00')
-    def test_find_available_reservations(self, reservation_0402, reservation_0301):
+    def test_find_available_reservations(self, reservation_0402: Reservation, reservation_0301: Reservation):
         repository = InMemoryReservationRepository()
+        usecase = FindAvailableReservationsUsecase(repository)
 
         cancelled_reservation = dataclasses.replace(reservation_0402,
                                                     id=ReservationId(str(uuid.uuid4())),
                                                     reservation_status=ReservationStatus.Canceled)
 
         repository.data[reservation_0402.id] = reservation_0402
-        repository.data[reservation_0301.id] = reservation_0301  # 過去の予約だよ
+        repository.data[reservation_0301.id] = reservation_0301
         repository.data[cancelled_reservation.id] = cancelled_reservation
 
         expected = [reservation_0402]
 
-        # TODO:ここに時間を入れるのは、どうかは、あとで考える
-        specification = AvailableReservationSpecification(datetime.datetime.now())
-
-        assert expected == repository.find_satisfying(specification)
+        assert usecase.find_available_reservations() == expected
