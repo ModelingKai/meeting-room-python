7f7a2bf6c9e4bbbd7f2518a80d5440b6a78527f4
mohira <shukatsudaison@gmail.com>
2020-05-31 22:48:57 +0900

    テスト用のデータビルダー DummyReservationBuilder をつくってみた

---
+++ tests/domain/reservation/dummy_reservation_builder.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+import dataclasses
+import datetime
+import uuid
+from dataclasses import dataclass
+from typing import Set
+
+from src.domain.employee.employee_id import EmployeeId
+from src.domain.meeting_room.meeting_room_id import MeetingRoomId
+from src.domain.reservation.number_of_participants import NumberOfParticipants
+from src.domain.reservation.reservation import Reservation
+from src.domain.reservation.reservation_id import ReservationId
+from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
+from src.domain.reservation.使用日時 import 使用日時
+
+
+@dataclass
+class DummyReservationBuilder:
+    """
+    発想: あくまで単体のReservationをつくるのを楽にするクラス
+
+    生成するReservationIdの重複は防いでいる
+
+
+    ReservationId以外の属性や、Reservation間の不正は検知はできない
+        - 会議室や、予約時間帯の被っている不正なデータをつくれてしまう
+        - 存在しない会議室IDや存在しない予約者IDもつくれる
+        - やろうと思えば、生成したReservationをrepositoryに登録して、ドメインサービスを適用するってことはできるが、やりすぎでは？
+
+    過去の日時を持つデータはつくれない
+        - 結局、使用日時クラスのnow で判断しているため
+        - with_time_range_to_reserve を使って差し込もうな
+    """
+    now: datetime.datetime
+    used_reservation_ids: Set[Reservation] = dataclasses.field(default_factory=set)
+
+    def __post_init__(self):
+        time_range_to_reserve = self._make_tomorrow_time_to_range()
+
+        self.dummy_reservation = Reservation(ReservationId('dummy_reservation_id'),
+                                             time_range_to_reserve,
+                                             NumberOfParticipants(4),
+                                             MeetingRoomId('A'),
+                                             EmployeeId('001'))
+
+    def _make_tomorrow_time_to_range(self) -> TimeRangeToReserve:
+        tomorrow = self.now + datetime.timedelta(days=1)
+
+        yyyy, mm, dd, *_ = tomorrow.timetuple()
+
+        return TimeRangeToReserve(使用日時(yyyy, mm, dd, 13, 00),
+                                  使用日時(yyyy, mm, dd, 14, 00))
+
+    def with_meeting_room_id(self, meeting_room_id: MeetingRoomId) -> DummyReservationBuilder:
+        # テストデータ作成のための強引なミューテーション
+        object.__setattr__(self.dummy_reservation, 'meeting_room_id', meeting_room_id)
+
+        return self
+
+    def with_cancel(self) -> DummyReservationBuilder:
+        self.dummy_reservation = self.dummy_reservation.cancel()
+
+        return self
+
+    def with_reserver_id(self, reserver_id: EmployeeId) -> DummyReservationBuilder:
+        # テストデータ作成のための強引なミューテーションだから妥協して使用している
+        object.__setattr__(self.dummy_reservation, 'reserver_id', reserver_id)
+
+        return self
+
+    def with_time_range_to_reserve(self, time_range_to_reserve: TimeRangeToReserve) -> DummyReservationBuilder:
+        # テストデータ作成のための強引なミューテーションだから妥協して使用している
+        object.__setattr__(self.dummy_reservation, 'time_range_to_reserve', time_range_to_reserve)
+
+        return self
+
+    def with_random_id(self) -> DummyReservationBuilder:
+        # テストデータ作成のための強引なミューテーションだから妥協して使用している
+        # ReservationId の生成ルールがガードできていないので注意！
+        object.__setattr__(self.dummy_reservation, 'id', ReservationId(str(uuid.uuid4())))
+
+        return self
+
+    def build(self) -> Reservation:
+        if self._has_already_build_reservation_id():
+            self.with_random_id()
+
+        self.used_reservation_ids.add(self.dummy_reservation.id)
+
+        return self.dummy_reservation
+
+    def _has_already_build_reservation_id(self) -> bool:
+        return self.dummy_reservation.id in self.used_reservation_ids
---
+++ tests/domain/reservation/test_dummy_reservation_builder.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import dataclasses
+import datetime
+
+import freezegun
+import pytest
+
+from src.domain.employee.employee_id import EmployeeId
+from src.domain.meeting_room.meeting_room_id import MeetingRoomId
+from src.domain.reservation.number_of_participants import NumberOfParticipants
+from src.domain.reservation.reservation import Reservation
+from src.domain.reservation.reservation_id import ReservationId
+from src.domain.reservation.reservation_status import ReservationStatus
+from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
+from src.domain.reservation.使用日時 import 使用日時
+from tests.domain.reservation.dummy_reservation_builder import DummyReservationBuilder
+
+
+class TestDummyReservationBuilder:
+    @pytest.fixture
+    @freezegun.freeze_time('2020-4-1 10:00')
+    def default_dummy_reservation(self) -> Reservation:
+        return Reservation(ReservationId('dummy_reservation_id'),
+                           TimeRangeToReserve(使用日時(2020, 4, 2, 13, 00), 使用日時(2020, 4, 2, 14, 00)),
+                           NumberOfParticipants(4),
+                           MeetingRoomId('A'),
+                           EmployeeId('001'))
+
+    @freezegun.freeze_time('2020-4-1 10:00')
+    def test_特に何も指定しないとただの翌日のReservationがつくれる(self, default_dummy_reservation: Reservation):
+        builder = DummyReservationBuilder(datetime.datetime.now())
+
+        assert isinstance(builder.build(), Reservation)
+
+    def test_IDはつくるたびに必ず変わる(self):
+        # 逆に言うとおなじIDをもつreservationは生成されない
+        builder = DummyReservationBuilder(datetime.datetime.now())
+
+        assert builder.build().id != builder.build().id
+
+    @freezegun.freeze_time('2020-4-1 10:00')
+    def test_キャンセル済みのReservationがつくれる(self, default_dummy_reservation: Reservation):
+        builder = DummyReservationBuilder(datetime.datetime.now())
+
+        expected = dataclasses.replace(default_dummy_reservation, reservation_status=ReservationStatus.Canceled)
+
+        assert builder.with_cancel().build() == expected
+
+    @freezegun.freeze_time('2020-4-1 10:00')
+    def test_会議室IDを指定できる(self, default_dummy_reservation: Reservation):
+        builder = DummyReservationBuilder(datetime.datetime.now())
+
+        another_meeting_room_id = MeetingRoomId('Z')
+
+        expected = dataclasses.replace(default_dummy_reservation, meeting_room_id=another_meeting_room_id)
+
+        assert builder.with_meeting_room_id(another_meeting_room_id).build() == expected
+
+    @freezegun.freeze_time('2020-4-1 10:00')
+    def test_予約者IDを指定できる(self, default_dummy_reservation: Reservation):
+        builder = DummyReservationBuilder(datetime.datetime.now())
+
+        another_reserver_id = EmployeeId('999')
+
+        expected = dataclasses.replace(default_dummy_reservation, reserver_id=another_reserver_id)
+
+        assert builder.with_reserver_id(another_reserver_id).build() == expected
+
+    @freezegun.freeze_time('2020-12-31 10:00')
+    def test_予約時間帯を指定できる(self, default_dummy_reservation: Reservation):
+        builder = DummyReservationBuilder(datetime.datetime(2020, 12, 31, 10, 00))
+
+        another_time_range_to_reserve = TimeRangeToReserve(使用日時(2020, 12, 31, 13, 00),
+                                                           使用日時(2020, 12, 31, 14, 00))
+
+        expected = dataclasses.replace(default_dummy_reservation, time_range_to_reserve=another_time_range_to_reserve)
+
+        assert builder.with_time_range_to_reserve(another_time_range_to_reserve).build() == expected
