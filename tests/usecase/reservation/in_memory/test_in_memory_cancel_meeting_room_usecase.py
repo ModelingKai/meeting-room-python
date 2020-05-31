bd9f9957a2f0b9da65a264d9810a2fab2e875ecc
mohira <shukatsudaison@gmail.com>
2020-05-31 23:11:06 +0900

    `tests/usecase/reservation/in_memory` のテスト内で DummyReservationBuilder を適用

--- tests/usecase/reservation/in_memory/test_in_memory_cancel_meeting_room_usecase.py
+++ tests/usecase/reservation/in_memory/test_in_memory_cancel_meeting_room_usecase.py
@@ -1,39 +1,26 @@
 import dataclasses
-import uuid
+import datetime
 
-import freezegun
 import pytest
 
-from src.domain.employee.employee_id import EmployeeId
-from src.domain.meeting_room.meeting_room_id import MeetingRoomId
-from src.domain.reservation.number_of_participants import NumberOfParticipants
 from src.domain.reservation.reservation import Reservation
-from src.domain.reservation.reservation_id import ReservationId
 from src.domain.reservation.reservation_status import ReservationStatus
-from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
-from src.domain.reservation.使用日時 import 使用日時
 from src.infrastructure.reservation.in_memory_reservation_repository import InMemoryReservationRepository
 from src.usecase.reservation.cancel_meeting_room_usecase import CancelMeetingRoomUsecase
 from src.usecase.reservation.errors import NotFoundReservationError
+from tests.domain.reservation.dummy_reservation_builder import DummyReservationBuilder
 
 
-@freezegun.freeze_time('2020-4-1 10:00')
 class TestInMemoryCancelMeetingRoomUsecase:
     def setup(self):
         self.reservation_repository = InMemoryReservationRepository()
         self.usecase = CancelMeetingRoomUsecase(self.reservation_repository)
 
     @pytest.fixture
-    @freezegun.freeze_time('2020-4-1 10:00')
     def reservation(self) -> Reservation:
-        """不正でないReservationインスタンスを作成するだけのfixture"""
-        return Reservation(ReservationId(str(uuid.uuid4())),
-                           TimeRangeToReserve(使用日時(2020, 4, 2, 13, 00), 使用日時(2020, 4, 2, 14, 00)),
-                           NumberOfParticipants(4),
-                           MeetingRoomId('A'),
-                           EmployeeId('001'))
+        return DummyReservationBuilder(datetime.datetime.now()).build()
 
-    def test_予約をキャンセルができること(self, reservation):
+    def test_予約をキャンセルができること(self, reservation: Reservation):
         expected = dataclasses.replace(reservation, reservation_status=ReservationStatus.Canceled)
 
         self.reservation_repository.data[reservation.id] = reservation
@@ -42,11 +29,11 @@ class TestInMemoryCancelMeetingRoomUsecase:
 
         assert expected == self.reservation_repository.data[reservation.id]
 
-    def test_存在しない予約に対してキャンセルするのはダメだよ(self, reservation):
+    def test_存在しない予約に対してキャンセルするのはダメだよ(self, reservation: Reservation):
         with pytest.raises(NotFoundReservationError):
             self.usecase.cancel_meeting_room(reservation.id)
 
-    def test_キャンセル済みに対してキャンセルするのもダメだよ(self, reservation):
+    def test_キャンセル済みに対してキャンセルするのもダメだよ(self, reservation: Reservation):
         canceled_reservation = dataclasses.replace(reservation, reservation_status=ReservationStatus.Canceled)
 
         self.reservation_repository.data[canceled_reservation.id] = canceled_reservation
--- tests/usecase/reservation/in_memory/test_in_memory_reserve_meeting_room_usecase.py
+++ tests/usecase/reservation/in_memory/test_in_memory_reserve_meeting_room_usecase.py
@@ -1,21 +1,18 @@
 import dataclasses
+import datetime
 import uuid
 
-import freezegun
 import pytest
 
-from src.domain.employee.employee_id import EmployeeId
-from src.domain.meeting_room.meeting_room_id import MeetingRoomId
 from src.domain.reservation.number_of_participants import NumberOfParticipants
 from src.domain.reservation.reservation import Reservation
 from src.domain.reservation.reservation_domain_service import ReservationDomainService
 from src.domain.reservation.reservation_id import ReservationId
 from src.domain.reservation.reservation_status import ReservationStatus
-from src.domain.reservation.time_range_to_reserve import TimeRangeToReserve
-from src.domain.reservation.使用日時 import 使用日時
 from src.infrastructure.reservation.in_memory_reservation_repository import InMemoryReservationRepository
 from src.usecase.reservation.errors import その会議室はその時間帯では予約ができませんよエラー
 from src.usecase.reservation.reserve_meeting_room_usecase import ReserveMeetingRoomUsecase
+from tests.domain.reservation.dummy_reservation_builder import DummyReservationBuilder
 
 
 class TestInMemoryReserveMeetingRoomUsecase:
@@ -25,22 +22,14 @@ class TestInMemoryReserveMeetingRoomUsecase:
         self.usecase = ReserveMeetingRoomUsecase(self.reservation_repository, domain_service)
 
     @pytest.fixture
-    @freezegun.freeze_time('2020-4-1 10:00')
     def reservation(self) -> Reservation:
-        """不正でないReservationインスタンスを作成するだけのfixture"""
-        return Reservation(ReservationId(str(uuid.uuid4())),
-                           TimeRangeToReserve(使用日時(2020, 4, 2, 13, 00), 使用日時(2020, 4, 2, 14, 00)),
-                           NumberOfParticipants(4),
-                           MeetingRoomId('A'),
-                           EmployeeId('001'))
+        return DummyReservationBuilder(datetime.datetime.now()).build()
 
-    @freezegun.freeze_time('2020-4-1 10:00')
     def test_会議室を予約する_正常系(self, reservation):
         self.usecase.reserve_meeting_room(reservation)
 
         assert reservation == self.reservation_repository.data[reservation.id]
 
-    @freezegun.freeze_time('2020-4-1 10:00')
     def test_会議室を予約する_異常系_会議室と予約時間帯が完全に被っている(self, reservation):
         # このテストクラスでは、予約時間帯が完全一致のテストケースしか要していないが、
         # 他のパターンは予約時間帯のテストケースでクリアしているので、特に不安はない
@@ -54,7 +43,6 @@ class TestInMemoryReserveMeetingRoomUsecase:
         with pytest.raises(その会議室はその時間帯では予約ができませんよエラー):
             self.usecase.reserve_meeting_room(new_reservation)
 
-    @freezegun.freeze_time('2020-4-1 10:00')
     def test_会議室を予約する_正常系_会議室と時間帯的には予約できないけどキャンセル済みだから予約できるんだなあ(self, reservation):
         exist_canceled_reservation = dataclasses.replace(reservation, reservation_status=ReservationStatus.Canceled)
 
